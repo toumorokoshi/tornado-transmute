a70a74c52150c807e1d65d04ef27392b4a4521de
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2015-11-24 22:45:59 -0800

    starting to build tornado-transmute.

---
+++ ubuild.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+
+def main(build):
+    build.packages.install(".", develop=True)
+
+
+def test(build):
+    main(build)
+    build.packages.install("jedi")
+    build.packages.install("sphinx")
+    build.packages.install("pytest")
+    build.packages.install("pytest-cov")
+    pytest = os.path.join(build.root, "bin", "py.test")
+    subprocess.call([
+        pytest, "--cov", "tornado_transmute",
+        "tornado_transmute/tests",
+        "--cov-report", "term-missing"
+    ])
+
+
+def _download_swagger_ui(build):
+    pass
+    # _script: "uscripts/download_swagger_ui.py"
